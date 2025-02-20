349b9345b3bc1bb9d5f7fc516a2d3e4c45930242
genki <genki569@gmail.com>
2025-02-21 03:35:02 +0900

    comit

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from app import db, login, app, cache
+from app import db, login, app
 import json
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
